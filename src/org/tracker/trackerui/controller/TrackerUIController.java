339538cfd7d964926a9b5a90b0c0b1a8da7c311e
yuri8 <yurilsilva93@gmail.com>
2023-12-24 18:25:32 -0300

    Button redesign

--- src/org/tracker/trackerui/controller/TrackerUIController.java
+++ src/org/tracker/trackerui/controller/TrackerUIController.java
@@ -1,21 +1,15 @@
 package org.tracker.trackerui.controller;
 
-import javafx.fxml.FXMLLoader;
 import javafx.scene.Node;
-import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.input.MouseEvent;
-import javafx.scene.layout.Pane;
 import javafx.scene.text.Text;
 import javafx.stage.FileChooser;
-import javafx.stage.Modality;
 import javafx.stage.Stage;
 import org.tracker.Tracker;
 import org.tracker.report.ReportType;
-import org.tracker.trackerui.TrackerUIApplication;
 
 import java.io.File;
-import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -25,10 +19,11 @@ public class TrackerUIController {
 
     public Button fafButton;
     public Button presumedButton;
-    public Button optionsButton;
+    public Button generateButton;
 
     private final List<File> presumedFiles = new ArrayList<>();
     private final List<File> fafFiles = new ArrayList<>();
+
 
     private boolean selectedPresumedFile = false;
     private boolean selectedFafFile = false;
@@ -44,19 +39,8 @@ public class TrackerUIController {
 
             if (fafFiles.isEmpty()) return;
 
-            fafButton.setText("Gerar");
             selectedFafFile = true;
-            optionsButton.setDisable(true);
             presumedButton.setDisable(true);
-
-        } else {
-            Tracker.createQueue(fafFiles);
-            Tracker.initialize(ReportType.FAF);
-            fafButton.setText("Selecionar");
-            selectedFafFile = false;
-            optionsButton.setDisable(false);
-            presumedButton.setDisable(false);
-            updateTotalValue();
         }
     }
 
@@ -71,36 +55,29 @@ public class TrackerUIController {
 
             if (presumedFiles.isEmpty()) return;
 
-            presumedButton.setText("Gerar");
             selectedPresumedFile = true;
-            optionsButton.setDisable(true);
             fafButton.setDisable(true);
+        }
+    }
 
-        } else {
+    public void onGenerateClicked(MouseEvent event) {
+        if (selectedPresumedFile) {
             Tracker.createQueue(presumedFiles);
             Tracker.initialize(ReportType.PRESUMED);
             presumedButton.setText("Selecionar");
             selectedPresumedFile = false;
-            optionsButton.setDisable(false);
             fafButton.setDisable(false);
             updateTotalValue();
+
+        } else if (selectedFafFile) {
+            Tracker.createQueue(fafFiles);
+            Tracker.initialize(ReportType.FAF);
+            fafButton.setText("Selecionar");
+            selectedFafFile = false;
+            presumedButton.setDisable(false);
+            updateTotalValue();
+
         }
-    }
-
-    public void onOptionsButtonClicked(MouseEvent event) throws IOException {
-        Stage primaryStage = (Stage) ((Node) event.getSource()).getScene().getWindow();
-
-        Stage stage = new Stage();
-        FXMLLoader fxmlLoader = new FXMLLoader(TrackerUIApplication.class.getResource("OptionsUI.fxml"));
-        Pane layout = fxmlLoader.load();
-
-        Scene scene = new Scene(layout, 600.0, 360.0);
-
-        stage.initModality(Modality.APPLICATION_MODAL);
-        stage.initOwner(primaryStage);
-        stage.setResizable(false);
-        stage.setScene(scene);
-        stage.showAndWait();
     }
 
     private List<File> createFileChooser(Stage stage) {
